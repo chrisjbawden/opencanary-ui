f0621a99d597e8c0f9e246ffb259b8fbc14f6da9
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2025-08-02 17:11:53 +1000

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) If /app is empty, populate it
+# If /app is empty, populate it
 if [ -z "$(ls -A /app 2>/dev/null)" ]; then
   echo "[init] seeding /app from image defaults"
   cp -a /opt/streamlit/. /app/
 fi
 
-# 2) If no credentials yet, seed them
+# If no credentials yet, seed them
 if [ ! -f /app/credentials.json ]; then
   echo "[init] creating blank credentials.json"
   cat > /app/credentials.json <<'EOF'
@@ -15,13 +15,13 @@ if [ ! -f /app/credentials.json ]; then
 EOF
 fi
 
-# 2.5) remove this to allow for the portscan functionality
+# remove this to allow for the portscan functionality
 
 if [ -f "/.dockerenv" ]; then
     rm -f "/.dockerenv"
 fi
 
-# 3) If no opencanary config yet, then create it
+# If no opencanary config yet, then create it
 if [ ! -f /etc/opencanaryd/opencanary.conf ]; then
   echo "[init] generating default opencanary.conf via opencanaryd"
   opencanaryd --copyconfig
@@ -31,7 +31,7 @@ if [ ! -f /etc/opencanaryd/opencanary.conf ]; then
   mv tmpconf /etc/opencanaryd/opencanary.conf
 fi
 
-# 4) rsyslog config: if user‐provided exists, install & restart; else stop rsyslog
+# rsyslog config: if user‐provided exists, install & restart; else stop rsyslog
 if [ -f /app/rsyslog-opencanary.conf ]; then
   echo "[init] custom rsyslog config found, installing"
   cp /app/rsyslog-opencanary.conf /etc/rsyslog.d/opencanary.conf
@@ -46,30 +46,34 @@ if [ -f /app/rsyslog-opencanary.conf ]; then
   fi
 fi
 
-# 4 rsyslog watchdog script
+#  rsyslog watchdog script
 echo "[start] launching rsyslog watchdog script"
 bash /app/rsyslog-watcher.sh > /dev/null 2>&1 &
 
-# 5 alerting script
+# alerting script
 echo "[start] launching log alert watchdog script"
 bash /app/alert.sh > /dev/null 2>&1 &
 
-# 5 alerting script
+# log management script
 echo "[start] log management script"
 bash /app/log_man.sh > /dev/null 2>&1 &
 
-# 6) Start OpenCanary in foreground mode but background it
+# Start a script to patch portscan function in foreground mode but background it
+echo "[start] portscan mod"
+bash /app/portscan_patch.sh > /dev/null 2>&1 &
+
+# Start OpenCanary in foreground mode but background it
 echo "[start] launching OpenCanary daemon"
 opencanaryd --start -f > /dev/null 2>&1 &
 
-# 7) Determine port from env or default to 8501
+# Determine port from env or default to 8501
 PORT="${MA_PORT:-8501}"
 
-# 8 Pick bind address: use BIND_ADDR if provided, else 0.0.0.0
+# Pick bind address: use BIND_ADDR if provided, else 0.0.0.0
 ADDRESS="${BIND_ADDR:-0.0.0.0}"
 echo "[entrypoint] Binding Streamlit to: $ADDRESS"
 
-# 9) Start Streamlit in foreground so Docker stays alive
+# Start Streamlit in foreground so Docker stays alive
 echo "[start] launching Streamlit app on port ${PORT}"
 exec streamlit run /app/app.py \
      --server.headless true \
